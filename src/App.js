8aef024302d28a10c395953ec6d9cfcd04dbf06e
João Pedro <pedroraimundodossantos13@gmail.com>
2022-02-22 12:46:30 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './components/pages/Home.js';
+import Company from './components/pages/Company.js';
+import Contact from './components/pages/Contact.js';
+import NewProject from './components/pages/NewProject.js';
+import Projects from './components/pages/Projects.js';
+
+import Container from './components/layout/Container.js'
+import Navbar from './components/layout/Navbar.js';
+import Footer from './components/layout/Footer.js';
+
+
+function App() {
+  return (
+    <Router>
+
+      <Navbar />
+      
+      <Container customClass='min-height'>
+        <Routes>
+        
+          <Route  exact path='/' element={<Home />} />
+
+          <Route ecxact path='/projects' element={<Projects />} />
+
+          <Route exact path='/company' element={<Company />} />
+
+          <Route exact path='/contact' element={< Contact />} />
+
+          <Route exact path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <Footer />
+
+    </Router>
+  );
+}
+
+export default App;
