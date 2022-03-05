5b15626091185dd45259116f52f1833204b7f8f3
João Pedro <pedroraimundodossantos13@gmail.com>
2022-03-05 20:21:14 -0300

    aula29

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -17,8 +17,11 @@ import {useState, useEffect} from 'react';
 function Projects(){
 
     const [projects, setProjects] = useState([]);
-
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
+
+
+
 
     const location = useLocation()
     let message = ''
@@ -46,6 +49,21 @@ function Projects(){
     }, [])
 
 
+    function removeProject(id){
+        fetch(`http://localhost:5000/projects/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type' : 'application/json'
+        }, 
+        }).then( resp => resp.json())
+        .then(() => {
+            setProjects(projects.filter( (project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch((err) => console.log(err))
+    }
+
+
     return(
         <div className={styles.project_container} >
             <div className={styles.title_container}>
@@ -54,6 +72,7 @@ function Projects(){
             </div>
 
             {message && <Message msg={message} type="success" />}
+            {projectMessage && <Message msg={projectMessage} type="error" />}
 
             <Container customClass='start'>
                 
@@ -66,6 +85,7 @@ function Projects(){
                     budget={project.budget}
                     category={project.category.name}
                     key={project.id}
+                    handleRemove={removeProject}
                     />
 
                 ))}
--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -4,6 +4,13 @@ import { Link } from "react-router-dom";
 import {BsPencil, BsFillTrashFill} from "react-icons/bs";
 
 function ProjectCard( {id, name, budget, category, handleRemove}){
+
+
+    const remove = (e) => {
+        e.preventDefault()
+        handleRemove(id)
+    }
+    
     return(
         <div className={styles.project_card}>
             <h4>{name}</h4>
@@ -22,7 +29,7 @@ function ProjectCard( {id, name, budget, category, handleRemove}){
                     <BsPencil /> Editar
                 </Link>
 
-                <button>
+                <button onClick={remove}>
                     <BsFillTrashFill /> Excluir
                 </button>
             </div>
